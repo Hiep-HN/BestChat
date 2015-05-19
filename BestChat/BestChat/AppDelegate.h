10fabcfceecf2322fdaff89f5cf55ef716806f8e
hiephuynhngoc <mr.hiepuit@gmail.com>
2015-05-19 16:06:01 +0700

    init project

---
+++ BestChat/BestChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestChat
+//
+//  Created by HiepHN-imac on 5/19/15.
+//  Copyright (c) 2015 Hiep Huynh Ngoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
